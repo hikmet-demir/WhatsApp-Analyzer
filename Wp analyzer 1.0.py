621916e898488398db9169b9a02f66f7f8cb0512
Hikmet <hikmet5548@gmail.com>
2018-10-28 19:52:51 +0300

    better code

--- Wp analyzer 1.0.py
+++ Wp analyzer 1.0.py
@@ -1,110 +1,141 @@
-with open('WhatsApp Chat with Başak.txt', 'r', encoding="utf8") as f:
-    read_data = f.read()
+import os
+from dateutil.parser import parse
+class WhatsAppAnalyzer:
 
-ff = open('outputFile.txt', 'w+', encoding="utf8")
+    def __init__(self):
+        self.file_name_input = "WhatsApp Chat with Oryantasyon’18 Saha Ekibi (1).txt"
+        self.file_name_output = "trial.txt"
+        self.people = {}
+        self.messages = []
+        self.input_lines = []
+        self.group = {}
+        self.read_data = ""
+        self.output_data = ""
+        self.d = {}
+        self.merged_lines = []
+
+    def get_files(self):
+
+        self.file_name_input = input("Enter your file name example ( trial.txt) : " ) 
+        print()
+        
+        with open(self.file_name_input,'r', encoding = "utf8") as f:
+            self.read_data = f.read()
+
+        self.input_lines = self.read_data.splitlines()
+
+        self.output_data = open(self.file_name_output,'w+',encoding ="utf8")
 
 
-d = {}
-people = {}
-lines = read_data.splitlines()
-merged_lines = []
-messages = []
+    def merge_real_line(self):
+        merge_lines = []
+        for j in self.input_lines:
+            if ('/') in j[0:10] and ',' in j[0:10]:
+                merge_lines.append(j)
+            else:
+                merge_lines[-1] = merge_lines[-1] + " " + j
+        
+        self.input_lines = merge_lines
 
-group = {}
+
+    def Merge_lines_func(self):
+        
+       # for i in self.input_lines:
+        #    print(i)
+        #    print("-")
+        
+        for line in self.input_lines:
+            if ':' in line:
+                temp_message = {}
+                temp_date = line.split('-')[0]
+                try:
+                    date = parse(temp_date)
+                    temp_message['date'] = date.date()
+                    temp_message['time'] = date.time()
+                    temp_message['minute'] = date.minute
+                    temp_message['hour'] = date.hour
+                    temp_message['day'] = date.day
+                    temp_message['month'] = date.month
+                    temp_message['year'] = date.year
+
+                    index1 = line.find(" - ")
+                    index2 = line.find(": ")
+                    person = line[index1+3:index2]
+                    temp_message['person'] = person
+
+                    message = line[index2+2:]
+                    temp_message['message'] = message
+                    self.messages.append(temp_message)
+                except:
+                    print(temp_date)
 
 
+    def find_message_counts(self):
+        for message in self.messages:
+            Person_name = message["person"]
+            if not Person_name in self.people:
+                self.people[ Person_name ] = {}
+                self.people[Person_name]["name"] = Person_name
+                self.people[Person_name]["Message_Count"] = 0
+                self.people[Person_name]["time"] = []
+                self.people[Person_name]["date"] = []
+                self.people[Person_name]["Night_owl"] = 0
+                self.people[Person_name]["Ice_breaker"] = 0
+            else:
+                self.people[Person_name]["Message_Count"] +=1
+                self.people[Person_name]["time"].append(message['time'])
+                self.people[Person_name]["date"].append(message['date'])
 
-def Merge_lines_func():
-    m_track = -1
-    for i in lines:
-        if( len(i) < 11 ):
-            merged_lines[m_track] += "\n"+  i
-        elif( i[2] == '/' and i[5] == '/' and i[10] == ',' ):
-            merged_lines.append(i);
-            m_track +=1
-        else:
-            merged_lines[m_track] += "\n"+  i
+    def Night_owl_count(self):
+        for person in self.people.keys():
+            for message_times in self.people[person]["time"]:
+                hour  = message_times.hour
+                if(hour <= 6):
+                    self.people[person]["Night_owl"] +=1
 
-    for i in merged_lines:
-        ff.write(i + "\n")
+    def Group_timing(self):
+        for i in range(0,24):
+            self.group[str(i)] = 0
+        for i in self.messages:
+            hour = i["time"].hour
+            self.group[str(hour)] += 1
 
-def find_message_counts():
-    for i in messages:
-        Person_name = i["from"]
-        if not Person_name in people:
-            people[ Person_name ] = {}
-            people[Person_name]["name"] = Person_name
-            people[Person_name]["Message_Count"] = 0
-            people[Person_name]["Message_Time"] = []
-            people[Person_name]["Message_date"] = []
-            people[Person_name]["Night_owl"] = 0
-            people[Person_name]["Ice_breaker"] = 0
-        people[Person_name]["Message_Count"] +=1
-        people[Person_name]["Message_Time"].append(i["time"])
-        people[Person_name]["Message_date"].append(i["date"])
 
-def Night_owl_count():
-    for i in people.keys():
-        for k in people[i]["Message_Time"]:
-            Message_Time_int = int(k[0:2])*60 + int(k[3])
-            if(Message_Time_int < 360):
-                people[i]["Night_owl"] +=1
+    def print_outputs(self):
+        t = []
+        for i in self.people.keys():
+            t.append((self.people[i]["Message_Count"],self.people[i]["Night_owl"], i ))
+        
+        t.sort(reverse=True)
+        counter = 0
+        for person in t:
+             print( "kişi adı: " +person[2]+ " message sayısı === " + str(person[0]) + "//// Night owl count == " + str(person[1]))
+             print()
+             counter +=1
+             if counter >10:
+                 break
+  
 
-def Group_timing():
-    for i in range(0,24):
-        group[str(i)] = 0
-    for i in messages:
-        hour = int(i["time"][0:2])
-        group[str(hour)] += 1
+        print("----")
+        print("Group Dynamics")
+        print("----")
 
-def create_messages():
-    for i in merged_lines:
-        temp = i
-        index1 = temp.find(" - ", 15, 25)
-        index2 = temp.find(": ", 15,70)
-        if(index2 == -1):
-            None
-        else:
-            temp_dict = {}
-            person = temp[ index1+3 : index2 ]
-            temp_dict["from"] = person
+        for i in range(0,24):
+            print("In time "+str(i)+":00-" + str(i) +":59 " + "  =  " + str(self.group[str(i)]) + " Message ")
 
-            index1 = temp.find(", ",0,30)
-            index2 = temp.find(" - ",0,40)
-            Message_Time = temp[ index1+2: index2]
-            temp_dict["time"] = Message_Time
+        """for i in messages:
+            print(i)"""
 
-            index1 = temp.find(": ")
-            Message_text = temp[index1+2:]
-            temp_dict["text"] = Message_text
 
-            index1 = temp.find(", ")
-            Message_date = temp[:index1]
-            temp_dict["date"] = Message_date
+    def main(self):
+        self.get_files()
+        self.merge_real_line()
+        self.Merge_lines_func()
+        self.find_message_counts()
+        self.Night_owl_count()
+        self.Group_timing()
+        self.print_outputs()
 
-            messages.append(temp_dict)
-
-def print_outputs():
-    for i in people.keys():
-        print( "kişi adı: " +i+ " message sayısı === " + str(people[i]["Message_Count"]) + "//// Night owl count == " + str(people[i]["Night_owl"]))
-
-    print("----")
-    print("Group Dynamics")
-    print("----")
-
-    for i in range(0,24):
-        print("In time "+str(i)+":00-" + str(i) +":59 " + "  =  " + str(group[str(i)]) + " Message ")
-
-    """for i in messages:
-        print(i)"""
-
-def main():
-    Merge_lines_func()
-    create_messages()
-    find_message_counts()
-    Night_owl_count()
-    Group_timing()
-    print_outputs()
 
 if __name__ == '__main__':
-    main()
+    WhatsAppAnalyzer().main()
